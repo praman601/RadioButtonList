ba226470c5623919e66a7fcfc68f591c2e0bd5b2
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 12:00:19 +0545

    another commit

---
+++ RadioButtonList/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RadioButtonList.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+            <asp:ListItem Text="red" Value="1"></asp:ListItem>
+            <asp:ListItem Text="green" Value="2"></asp:ListItem>
+      <asp:ListItem Text="yellow" Value="3"></asp:ListItem>
+            <asp:ListItem text="blue" Value="4"></asp:ListItem>
+        </asp:RadioButtonList>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Select" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Clear Selection" Width="98px" />
+    </form>
+</body>
+</html>
---
+++ RadioButtonList/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButtonList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (RadioButtonList1.SelectedValue != null)
+            {
+
+
+                Response.Write("Text= " + RadioButtonList1.SelectedItem.Text + "<br/>");
+                Response.Write("value= " + RadioButtonList1.SelectedItem.Value + "<br/>");
+                Response.Write("index= " + RadioButtonList1.SelectedIndex.ToString());
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            RadioButtonList1.SelectedIndex = -1;
+        }
+    }
+}
